3c80e137ca5398aeb8e1eeecc9c2866c79e755b6
Gabriel <gabr.cuevas@duocuc.cl>
2025-10-19 22:06:53 -0300

    arregle un problema con el slide

--- src/Components/organisms/ProductSlider/ProductSlider.jsx
+++ src/Components/organisms/ProductSlider/ProductSlider.jsx
@@ -5,36 +5,36 @@ import './ProductSlider.css';
 
 const productsData = [
   {
-    id: 1,
+    id: 5,
     name: 'PlayStation 5',
     price: '$549.990 CLP',
     description: 'La nueva generación de consolas.',
     image: 'https://static2.pisapapeles.net/uploads/2024/09/GXH7fzoXIAAqLGh.jpeg',
-    linkTo: '/producto/1',
+    linkTo: '/producto/5',
   },
   {
-    id: 2,
+    id: 4,
     name: 'Auriculares Gamer HyperX Cloud II',
     price: '$79.990 CLP',
     description: 'Audio inmersivo para una experiencia de otro nivel.',
     image: 'https://cdn.shopify.com/s/files/1/0477/6368/5540/files/12.png?v=1648130543',
-    linkTo: '/producto/2',
+    linkTo: '/producto/4',
   },
   {
-    id: 3,
+    id: 8,
     name: 'Mouse Gamer Logitech G502 HERO',
     price: '$49.990 CLP',
     description: 'Precisión y velocidad para el gamer competitivo.',
     image: 'https://jasaltec.cl/wp-content/uploads/2022/01/Screenshot_7-4.jpg',
-    linkTo: '/producto/3',
+    linkTo: '/producto/8',
   },
   {
-    id: 4,
+    id: 7,
     name: 'Silla Gamer Secretlab Titan',
     price: '$349.990 CLP',
     description: 'Comodidad ergonómica para largas sesiones de juego.',
     image: 'https://images.secretlab.co/subimage/tr:n-display_gallery/M07-E24PU-STELH1R_Hero.jpg',
-    linkTo: '/producto/4',
+    linkTo: '/producto/7',
   },
 ];
 
