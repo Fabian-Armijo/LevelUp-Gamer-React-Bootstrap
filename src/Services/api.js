cfc0ee2ac8ae8ba08adfc82dc69d37882bac9429
Gabriel <gabriel.acv0504@gmail.com>
2025-11-26 14:11:08 -0300

    arreglo de erro con las estadisticas admin

--- src/Services/api.js
+++ src/Services/api.js
@@ -6,15 +6,20 @@ const api = axios.create();
 // ¡LA MAGIA! Esto se ejecuta ANTES de CUALQUIER petición
 api.interceptors.request.use(
   (config) => {
-    // 1. Busca el token en el almacenamiento local
     const token = localStorage.getItem('token');
     
-    // 2. Si el token existe, lo pone en la cabecera 'Authorization'
+    // AGREGA ESTO PARA VERIFICAR EN CONSOLA
+    console.log("Interceptor activado para:", config.url);
+    console.log("Token recuperado del storage:", token);
+
     if (token) {
       config.headers['Authorization'] = `Bearer ${token}`;
+      console.log("Header Authorization agregado exitosamente.");
+    } else {
+      console.warn("¡ALERTA! No se encontró token en localStorage bajo la clave 'token'.");
     }
     
-    return config; // Deja que la petición continúe
+    return config;
   },
   (error) => {
     // Si hay un error, rechaza la promesa
